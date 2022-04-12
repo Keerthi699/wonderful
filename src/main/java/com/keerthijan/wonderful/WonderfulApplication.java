5285c7527264d6cbb1a824b853ddc3941813a8e3
keerthi <keerthij62@gmail.com>
2022-04-12 16:33:08 +0530

    updated spotless plugin

--- src/main/java/com/keerthijan/wonderful/WonderfulApplication.java
+++ src/main/java/com/keerthijan/wonderful/WonderfulApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WonderfulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WonderfulApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WonderfulApplication.class, args);
+    }
 }
--- src/test/java/com/keerthijan/wonderful/WonderfulApplicationTests.java
+++ src/test/java/com/keerthijan/wonderful/WonderfulApplicationTests.java
@@ -6,9 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WonderfulApplicationTests {
 
-	@Test
-	@Ignore
-	void contextLoads() {
-	}
-
+    @Test
+    @Ignore
+    void contextLoads() {}
 }
