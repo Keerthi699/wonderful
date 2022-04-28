883424bc5bb498d3c7464bdf70f73ccd6116f7e2
keerthi <keerthij62@gmail.com>
2022-04-28 11:39:54 +0530

    Added for loop

--- src/main/java/com/keerthijan/wonderful/service/impl/UserServicesImpl.java
+++ src/main/java/com/keerthijan/wonderful/service/impl/UserServicesImpl.java
@@ -14,29 +14,43 @@ public class UserServicesImpl implements UserService {
     @Override
     public String getWelcomeMessage(UserDetailsDTO userDetailsDTO) {
         String welcome = "";
-        List<String> Country = getCountryList();
-        if (Country.contains(userDetailsDTO.getLocation())) {
-            welcome =
-                    " Namaste "
-                            + userDetailsDTO.getName()
-                            + ", glad you are in "
-                            + userDetailsDTO.getCompany();
-        } else {
-            welcome =
-                    "Hello"
-                            + userDetailsDTO.getName()
-                            + ", glad you are in "
-                            + userDetailsDTO.getCompany()
-                            + "We don't have your Location";
+        List<UserDetailsDTO> userDetails = getUserDetails();
+        for (int i = 0; i < userDetails.size(); i++) {
+            if (userDetails.get(i).getLocation().equals(userDetailsDTO.getLocation())){
+                welcome = welcome + userDetails.get(i).getName() + userDetails.get(i).getLocation();
+            }
         }
+        // if (Country.contains(userDetailsDTO.getLocation())) {
+        //    welcome =
+        //             " Namaste "
+        //                    + userDetailsDTO.getName()
+        //                  + ", glad you are in "
+        //                  + userDetailsDTO.getCompany();
+        //} else {
+        //  welcome =
+        //          "Hello"
+        //                  + userDetailsDTO.getName()
+        //                  + ", glad you are in "
+        //                  + userDetailsDTO.getCompany()
+        //                  + "We don't have your Location";
+        //}
         return welcome;
     }
 
-    private List<String> getCountryList() {
-        List<String> countries = new ArrayList<String>();
-        countries.add("India");
-        countries.add("U.S");
-        countries.add("U.K");
-        return countries;
+    private List<UserDetailsDTO> getUserDetails() {
+
+        List<UserDetailsDTO> userDetails = new ArrayList<UserDetailsDTO>();
+        userDetails.add(0, new UserDetailsDTO(name:  "Keerthi", company: "ArchisAcademy", location: "India"));
+        userDetails.add(1, new UserDetailsDTO(name: "Sruthi", company: "Vaichitralaya", location: "France"));
+        userDetails.add(2, new UserDetailsDTO(name: "Siva", company: "Medico", location: "U.K" ));
+        return userDetails;
     }
 }
+    //private List<String> getCountryList() {
+    //  List<String> countries = new ArrayList<String>();
+    //    countries.add("India");
+    //    countries.add("U.S");
+    //  countries.add("U.K");
+    //  return countries;
+   // }
+   //}
